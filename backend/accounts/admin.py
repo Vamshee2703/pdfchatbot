a4b822c718924ce884ac1d0295ebc18160712e3d
VAMSHEE REDDY <jeeruvamshee2703@gmail.com>
2026-04-10 12:23:23 +0530

    Resolved merge conflicts

--- backend/accounts/admin.py
+++
@@ -1,6 +0,0 @@
-from django.contrib import admin
-from .models import User, ChatMessage, WebsiteChunk
-
-admin.site.register(User)
-admin.site.register(ChatMessage)
-admin.site.register(WebsiteChunk)
--- backend/accounts/services/content.py
+++
@@ -1,3 +0,0 @@
-from .crawler import crawl_full_website
-
-COMPANY_CONTENT = crawl_full_website()
